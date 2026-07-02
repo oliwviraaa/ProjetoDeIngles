c5163a6acc3080a0b0640eb1b947349ba815d312
Giovanna Vasconcelos <ncelosgiovanna@gmail.com>
2026-07-02 11:54:54 -0300

    Adiciona opcoes do arquivo explicacoesA2.py

---
+++ explicacoesA2.py
@@ -0,0 +1,31 @@
+def mostrar_conteudos(letra, nivel):
+    if nivel == 2:
+        match letra:
+            case "A": 
+                print("PAST SIMPLE (REGULAR/IRREGULAR)")
+            case "B": 
+                print("PAST CONTINUOUS")
+            case "C": 
+                print("FUTURE WITH GOING TO")
+            case "D": 
+                print("FUTURE WITH WILL")
+            case "E": 
+                print("COMPARATIVOS E SUPERLATIVOS")
+            case "F": 
+                print("MODAIS DE OBRIGAÇÃO E CONSELHO (MUST/SHOULD)")
+            case "G": 
+                print("PRONOMES DE OBJETO (ME, YOU, HIM...)")
+            case "H": 
+                print("ADVÉRBIOS DE FREQUÊNCIA")
+            case "I": 
+                print("QUANTIFICADORES (MUCH, MANY, A LOT OF)")
+            case "J": 
+                print("SUBSTANTIVOS CONTÁVEIS E INCONTÁVEIS")
+            case "K": 
+                print("INFINITIVO E GERÚNDIO (LIKE/HATE + -ING)")
+            case "L": 
+                print("PREPOSIÇÕES DE TEMPO E MOVIMENTO")
+            case "M": 
+                print("FIRST CONDITIONAL (INTRODUÇÃO)")
+            case "N": 
+                print("VOCABULÁRIOS EXPANDIDOS (AEROPORTO, COMPRAS, CLIMA)")
