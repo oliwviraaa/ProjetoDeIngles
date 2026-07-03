f06adb5b01884a929dbd3ebdb24190699aef5c5d
Giovanna Vasconcelos <ncelosgiovanna@gmail.com>
2026-07-03 18:32:53 -0300

    Adiciona opcoes do arquivo explicacoesB1.py

---
+++ explicacoesB1.py
@@ -0,0 +1,32 @@
+def mostrar_conteudos(letra, nivel):
+    if nivel== 3:
+        match letra:
+            case "A":
+                print("PRESENT PERFECT SIMPLE")
+            case "B":
+                print("PRESENT PERFECT VS PAST SIMPLE")
+            case "C":
+                print("PAST PERFECT SIMPLE ")
+            case "D":
+                print("FUTURE CONTINUOUS")
+            case "E":
+                print("MODAIS DE PROBABILIDADE (May, Might, Could)")
+            case "F":
+                print("FIRST AND SECOND CONDITIONALS")
+            case "G":
+                print("Reported Speech (Introdução)")
+            case "H":
+                print("PASSIVE VOICE (Present/Past Simple)")
+            case "I":
+                print("RELATIVE CLAUSES (Who, Which, That, Where)")
+            case "J":
+                print("Used to (para hábitos passados)")
+            case "K":
+                print("Questions Tags")
+            case "L":
+                print("Phrasal Verbs básicos e comuns")
+            case "M":
+                print("Conectivos (Although, Howe)")
+            case "N":
+                print("Vocabulários intermediários (Trabalho, Dinheiro, Opiniões)")
+
