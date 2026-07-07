d9fa93fa6e501cbb011e93392c760fff2756c5f1
Giovanna Vasconcelos <ncelosgiovanna@gmail.com>
2026-07-07 18:29:13 -0300

    Adiciona opcoes do arquivo explicacoesB2.py

---
+++ explicacoesB2.py
@@ -0,0 +1,31 @@
+def mostrar_explicacoes(letra, nivel):
+    if nivel== 4:
+        match letra:
+            case "A":
+                print("PRESENT PERFECT CONTINUOUS")
+            case "B":
+                print("PAST PERFECT VS CONTINUOUS")
+            case "C":
+                print("FUTURE PERFECT (Simple e Continuous)")
+            case "D":
+                print("PASSIVE VOICE AVANÇADA (Com verbos modais e estruturas impessoais)")
+            case "E":
+                print("THIRD CONDITIONAL E MIXED CONDITIONALS")
+            case "F":
+                print("WISH/IF ONLY (para arrependimento ou desejo)")
+            case "G":
+                print("MODAIS DE DEDUÇÃO NO PASSADO (Must have, Might have, Can't have)")
+            case "H":
+                print("REPORTED SPEECH avançado (verbos de relato como Deny, Admit, Accuse)")
+            case "I":
+                print("RELATIVE CLAUSES RESTRITIVAS E NÃO RESTRITIVAS (com preposições")
+            case "J":
+                print("GERÚNDIO E INFINITIVO avançados (verbos que mudam de sentido)")
+            case "J":
+                print("INVERSÃO COM ADVÉRBIOS NEGATIVOS (estruturas formais)")
+            case "L":
+                print("PHRASAL VERBS avançados e idiomáticos")
+            case "M":
+                print("CONECTIVOS AVANÇADOS DE COESÃO (Furthermore, Whereas, On the contrary)")
+            case "N":
+                print("Vocabulário corporativo, debates formais e expressões idiomáticas")
